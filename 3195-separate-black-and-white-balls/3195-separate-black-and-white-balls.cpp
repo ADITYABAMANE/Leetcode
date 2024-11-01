6210d61a58a4111bc96d7bbc4019cad9f643f852
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-11-01 11:44:33 +0530

    Time: 3 ms (86.19%), Space: 15.6 MB (40.73%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        long long swap=0;
         int black=0;
+        long  swap=0;
         for(int i=0;i<s.length();i++){
+
             if(s[i]=='0'){
-                swap+=(long long)black;
+                swap+=black;
 
             }
             else{
@@ -13,6 +14,7 @@ public:
             }
         }
         return swap;
+        
         
     }
 };
