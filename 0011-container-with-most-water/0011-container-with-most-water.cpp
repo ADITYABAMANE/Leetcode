d5f9c15fa0667cd8d1f086dc182ad1123e71554e
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-11-10 18:27:06 +0530

    Time: 0 ms (100%), Space: 61.7 MB (32.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,22 +5,19 @@ public:
         int left=0;
         int right=n-1;
         int max_water=0;
-
-        int max_area=0;
-        while(left<right){
-            int max_area=(right-left)*min(height[left],height[right]);
-            max_water=max(max_water,max_area);
-
-            if(height[left]<height[right]){
-                left++;
-
-            }
-            else{
+        int area=0;
+        while(left<right ){
+            area=min(height[left],height[right])*(right-left);
+             max_water=max(max_water,area);
+            if(height[left]>height[right]){
                 right--;
             }
-            
+            else{
+                left++;
+            }
+           
+
         }
-        return  max_water;
-        
+        return max_water;
     }
 };
