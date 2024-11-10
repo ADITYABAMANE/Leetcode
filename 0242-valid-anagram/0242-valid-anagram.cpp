4aaef8c64461dfa1393c1ac8477f0416a765d745
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-11-10 12:47:59 +0530

    Time: 10 ms (6.12%), Space: 8.8 MB (55.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
-        map<char, int>m1;
-        
-        for(char ch:s){
-            m1[ch]++;
-
-        }
-        for(char ch1:t){
-            m1[ch1]--;
-
-        }
-        for(auto d:m1){
-            if(d.second!=0){
-                return false;
-            }
-           
-
-        }
-        return true;
-
-
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t)return true;
+        return false;
         
     }
 };
