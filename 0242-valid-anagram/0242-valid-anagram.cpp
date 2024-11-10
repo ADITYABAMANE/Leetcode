c376be43713dfbde2e96f82dc44d68b2f33fd1f9
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-11-10 13:24:12 +0530

    Time: 7 ms (37.63%), Space: 10.1 MB (16.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)return true;
-        return false;
+        map<char,int>mp1;
+        map<char,int>mp2;
+        for(char c1:s){
+            mp1[c1]++;
+        }
+        for(char c2:t){
+            mp1[c2]--;
+        }
+        for(auto it:mp1){
+            if(it.second!=0){
+                return false;
+
+            }
+        }
+        return true;
+
         
     }
 };
