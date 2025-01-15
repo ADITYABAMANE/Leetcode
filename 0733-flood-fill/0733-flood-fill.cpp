5f517ff6967d0a2c4efc6600ff2ae3d589a417b4
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-01-15 17:34:44 +0530

    Time: 0 ms (100%), Space: 18.1 MB (64.52%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,35 +1,38 @@
 class Solution {
 private:
-    void dfs(int row, int col, vector<vector<int>>& ans, const vector<vector<int>>& image, int newcolor, int iniColor, int delrow[], int delcol[]) {
-        ans[row][col] = newcolor;  
 
-        int n = image.size();
-        int m = image[0].size();
-
-        
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-
-           
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == iniColor && ans[nrow][ncol] != newcolor) {
-                dfs(nrow, ncol, ans, image, newcolor, iniColor, delrow, delcol);
-            }
+ void dfs(int row,int col,vector<vector<int>>& ans,vector<vector<int>>& image, int &color, int delrow[], int delcol[],int &inicolor){
+    ans[row][col]=color;
+    int n=image.size();
+    int m=image[0].size();
+    for(int i=0;i<4;i++){
+        int nrow=row+delrow[i];
+        int ncol=col+delcol[i];
+        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]==inicolor && ans[nrow][ncol]!=color){
+             dfs(nrow,ncol,ans,image,color, delrow, delcol,inicolor);
         }
+       
+
     }
+ }
+
+
+
+
+
 
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newcolor) {
-        int iniColor = image[sr][sc];
+     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int inicolor=image[sr][sc];
+        vector<vector<int>>ans=image;
+        
+        int delrow[]={-1,0,+1,0};
+        int delcol[]={0,+1,0,-1};
+        dfs(sr,sc,ans,image,color,  delrow,  delcol,inicolor);
+         return ans;
+
+
 
         
-        if (iniColor == newcolor) return image;
-
-        vector<vector<int>> ans = image;  
-        int delrow[] = {-1, 0, +1, 0};    
-        int delcol[] = {0, +1, 0, -1};    
-
-        dfs(sr, sc, ans, image, newcolor, iniColor, delrow, delcol);
-        return ans;
     }
 };
