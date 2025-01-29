d93dbed57009d517dd53a5c96864a32b70994d5f
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-01-29 17:36:43 +0530

    Time: 267 ms (13.96%), Space: 161.6 MB (21.41%) - LeetHub

--- 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
@@ -1,31 +1,34 @@
 class Solution {
-    
+    int mini=0;
+    int mx=0;
+    public:
+
+    void dfs(int mmanager, vector<int>& informTime, unordered_map<int,vector<int>>&mp ){
+        mx=max(mx,mini);
+        for(auto submanager:mp[mmanager]){
+            mini+=informTime[mmanager];
+            dfs(submanager,informTime,mp);
+            mini-=informTime[mmanager];
+        }
+
+    }
+
+
+
 public:
-int ans=0;
-int mx=0;
-    void dfs(int mainman,vector<int>& informTime, unordered_map<int,vector<int>>&mp){
-        mx=max(mx,ans);
-
-        for(auto  emploee:mp[mainman]){
-            ans+=informTime[mainman];
-            dfs(emploee,informTime,mp);
-            ans-=informTime[mainman];
-        }
-
-    }
-
-
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        unordered_map<int,vector<int>>mp;
 
-        for(int i=0;i<n;i++){
-            if(manager[i]!=-1){
-                mp[manager[i]].push_back(i);
-            }
+     unordered_map<int,vector<int>>mp;   
+
+     //to make a adjacency list
+     for(int i=0;i<n;i++){
+        if(manager[i]!=-1){
+            mp[manager[i]].push_back(i);
+
         }
-        dfs(headID,informTime,mp);
-        return mx;
-
-        
+     }
+     dfs(headID,informTime,mp);
+     return mx;
     }
+
 };
