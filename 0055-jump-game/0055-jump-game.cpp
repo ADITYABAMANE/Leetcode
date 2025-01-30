6ed60ffc61c2cf390250c24009bec99072a098b1
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-01-30 20:16:23 +0530

    Time: 0 ms (100%), Space: 52.3 MB (30.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int minj=0;
+        
         int n=nums.size();
+        int minj=0;
         for(int i=n-2;i>=0;i--){
             minj++;
             if(nums[i]>=minj){
@@ -11,7 +12,5 @@ public:
         }
         if(minj==0)return true;
         return false;
-        
-
     }
 };
