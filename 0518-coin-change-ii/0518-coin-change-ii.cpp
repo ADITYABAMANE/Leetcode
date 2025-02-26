d6ea46507cc434813f283abc79a92a246fe1b4ba
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-02-26 11:53:15 +0530

    Time: 15 ms (78.01%), Space: 28.6 MB (45.14%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
+  long f(int ind, int target,vector<int>& coins, vector<vector<int>>&dp){
 
-long f(int ind,int target,vector<int>& coins, vector<vector<int>>&dp){
-    if(target==0){
-        return 1;
+    //base cases
+    if(target==0)return 1;
+    if(ind<0)return 0;
+
+    if(dp[ind][target]!=-1){
+        return dp[ind][target];
     }
-    if(ind<0){
-        return 0;
-    }
-    if(dp[ind][target]!=-1)return dp[ind][target];
-    long nottake=f(ind-1, target,coins,dp);
+
     long take=0;
     if(coins[ind]<=target){
-        take=f(ind,target-coins[ind],coins,dp);}
+        take=f(ind,target-coins[ind],coins,dp);
+
+    }
+    long nottake=f(ind-1,target,coins,dp);
 
     return dp[ind][target]=take+nottake;
 
-}
+  }
     int change(int amount, vector<int>& coins) {
-            int n=coins.size();
-            vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
         return f(n-1,amount,coins,dp);
+
+        
     }
 };
