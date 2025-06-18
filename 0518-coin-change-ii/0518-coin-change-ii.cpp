0d1dad44dd98a1dbd6fee2e102d80ec425a12249
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-06-18 16:47:29 +0530

    Time: 22 ms (46.19%), Space: 28.4 MB (45.53%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-  long f(int ind, int target,vector<int>& coins, vector<vector<int>>&dp){
 
-    //base cases
-    if(target==0)return 1;
-    if(ind<0)return 0;
+    long noofcoins(int ind, int target,vector<int>& coins, vector<vector<int>>&dp ){
 
-    if(dp[ind][target]!=-1){
-        return dp[ind][target];
+        if(ind<0)return 0;
+        if(target==0)return 1;
+        long take=0;
+        if(dp[ind][target]!=-1){
+            return dp[ind][target];
+        }
+        if(coins[ind]<=target){
+            take=noofcoins(ind, target-coins[ind],coins,dp);
+        }
+        long nottake=noofcoins(ind-1, target,coins,dp);
+
+        return dp[ind][target]=take+nottake;
+
     }
-
-    long take=0;
-    if(coins[ind]<=target){
-        take=f(ind,target-coins[ind],coins,dp);
-
-    }
-    long nottake=f(ind-1,target,coins,dp);
-
-    return dp[ind][target]=take+nottake;
-
-  }
-    int change(int amount, vector<int>& coins) {
-
+    int change(int amount,vector<int>& coins) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return f(n-1,amount,coins,dp);
+        
+        return noofcoins(n-1,amount, coins,dp);
+
+
+
+
 
         
     }
