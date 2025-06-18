cc07f8f51311bcd6bc29445e78370cbb79f53aad
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-06-18 22:32:59 +0530

    Time: 57 ms (16.84%), Space: 82.1 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-     long noofcoins(int ind, int target, vector<int>& coins) {
-        if (ind < 0) return INT_MAX;
-        if (target == 0) return 1;
+     long noofcoins(int ind, int target, vector<int>& coins,vector<vector<long>>&dp) {
+        if (ind ==0) {
+        if( target%coins[0]==0){
+            return target/coins[0];
+        }
+        return INT_MAX;}
 
-        long take = 0;
-        if (coins[ind] <= target)
-            take = 1+ noofcoins(ind, target - coins[ind], coins);  // take same coin again
+        if(dp[ind][target]!=-1){
+            return dp[ind][target];
+        }
 
-        long nottake = noofcoins(ind - 1, target, coins);       // skip current coin
         
-        return min(take , nottake);
+        long nottake=0+ noofcoins(ind-1,target,coins,dp);
+        long take=INT_MAX;
+        if (coins[ind] <= target)
+            take = 1+ noofcoins(ind, target - coins[ind], coins,dp);  // take same coin again
+
+       
+
+        return dp[ind][target]= min(take , nottake);
     }
     int coinChange(vector<int>& coins, int amount) {
          int n = coins.size();
-        return noofcoins(n - 1, amount, coins);
+        vector<vector<long>> dp(n, vector<long>(amount + 1, -1));
+        long ans= noofcoins(n - 1, amount, coins,dp);
+        if(ans>=INT_MAX)return -1;
+        return ans;
     }
 };
