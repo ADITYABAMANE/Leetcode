70e1ee2ffaa757a64a4bf797c500c0157bb851ea
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-06-26 00:44:45 +0530

    Time: 13 ms (13.21%), Space: 20.2 MB (31.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n=numCourses;
-       vector<vector<int>>adj(n);
-        
-        //make the adjacency list
+        vector<vector<int>>adj(n);
+
+        //building the adjacency list
         for(auto it:prerequisites){
             adj[it[1]].push_back(it[0]);
-
         }
 
-        //implement topo sort and then store the topo sort
-        vector<int>indegree(n,0);
-        //fill the indegree in the below step;
-
-        for(int i=0;i<n;i++){
-            for(auto it:adj[i]){
-                indegree[it]++;
-            }
+        //calculate indegrre of each node
+        vector<int>indegree(n);
+       for(int i=0;i<n;i++){
+        for(auto it:adj[i]){
+            indegree[it]++;
         }
-
+       }
+       //check if indegree zero,,,then push the node into the ququq
         queue<int>q;
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
@@ -29,24 +26,20 @@ public:
         vector<int>topo;
         while(!q.empty()){
             int node=q.front();
-            q.pop();
             topo.push_back(node);
-            //after the node is removed from the queue reduce the indegree of the adjacent nodes of the node
+            q.pop();
+
+
             for(auto it:adj[node]){
                 indegree[it]--;
-
                 if(indegree[it]==0){
                     q.push(it);
                 }
             }
         }
+
         if(topo.size()==n)return true;
-
         return false;
-
-
-
-
 
         
     }
