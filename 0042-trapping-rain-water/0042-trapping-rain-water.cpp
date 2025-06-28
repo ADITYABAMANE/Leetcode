8a969b859b256c840419cbddab28312441b79bd3
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-06-28 19:30:48 +0530

    Time: 0 ms (100%), Space: 25.9 MB (79.32%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,28 +4,27 @@ public:
         int n=height.size();
         int left=0;
         int right=n-1;
+        int water=0;
+
         int leftmax=height[left];
         int rightmax=height[right];
-         int water=0;
         while(left<right){
-           
-
             if(leftmax<rightmax){
                 left++;
                 leftmax=max(leftmax,height[left]);
                 water+=leftmax-height[left];
 
-
             }
             else{
                 right--;
                 rightmax=max(rightmax,height[right]);
-                water+=rightmax-height[right];
-
+                water+=rightmax-height[right];      
 
             }
         }
         return water;
+
+
 
         
     }
