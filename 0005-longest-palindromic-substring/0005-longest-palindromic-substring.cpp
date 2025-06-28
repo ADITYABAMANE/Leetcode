9a3407d10e1342878f1f23e829c05fac8539cb77
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-06-29 01:27:26 +0530

    Time: 841 ms (12.28%), Space: 9.5 MB (69.7%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,48 @@
 class Solution {
-private:
-    bool ispalindrome(string& stz, int i, int j) {
-        while (i < j) {
-            if (stz[i] != stz[j]) {
+    private:
+    bool ispalindrome(string &stz,int i,int j){
+        int n=stz.length();
+        
+
+        // i=0;
+        // j=n-1;
+        while(i<j){
+            if(stz[i]!=stz[j]){
                 return false;
             }
             i++;
             j--;
         }
         return true;
+
     }
-
 public:
-    int ini = -1;
-
+int ini=-1;
     string longestPalindrome(string s) {
-        int n = s.length();
-        int maxlen = 0;
 
-        for (int start = 0; start < n; start++) {
-            for (int end = start; end < n; end++) {
-                if (ispalindrome(s, start, end)) {
-                    if (end - start + 1 > maxlen) {
-                        maxlen = end - start + 1;
-                        ini = start;
-                    }
+        int n=s.length();
+       
+        
+        int maxlen=0;
+        for(int start=0;start<n;start++){
+           
+            for(int end=start;end<n;end++){
+              
+               if(ispalindrome(s,start,end)){
+                if(end-start+1>maxlen){
+                    maxlen=end-start+1;
+                    ini=start;
+
+
+
+
                 }
-            }
-        }
 
-        return s.substr(ini, maxlen);
+               } 
+            }
+
+        }
+        return s.substr(ini,maxlen);
+        
     }
 };
