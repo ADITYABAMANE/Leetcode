bbc72014e2520dddbe797feef88b2eef60c46cad
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-06-28 21:06:19 +0530

    Time: 0 ms (100%), Space: 12.5 MB (8.93%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>temp;
-        for(int i=0;i<m;i++){
+        vector<int> temp;
+
+        for (int i = 0; i < m; i++) {
             temp.push_back(nums1[i]);
         }
-        for(int j=0;j<n;j++){
+
+        for (int j = 0; j < n; j++) {
             temp.push_back(nums2[j]);
         }
 
-        sort(temp.begin(),temp.end());
-        // nums1.clear();
-        int s=temp.size();
-        for(int z=0;z<s;z++){
-            nums1[z]=temp[z];
+        sort(temp.begin(), temp.end());
 
+        nums1.clear();  // Only works if you're allowed to resize nums1
+
+        for (int z = 0; z < temp.size(); z++) {
+            nums1.push_back(temp[z]);
         }
-        
     }
 };
