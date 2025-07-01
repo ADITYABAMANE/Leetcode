3b3cbb2d3314fdb9695ce4e12baaae9586525e8b
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-07-01 11:04:14 +0530

    Time: 6 ms (54.71%), Space: 20.3 MB (23.65%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
         int n=numCourses;
         vector<vector<int>>adj(n);
-
-        //building the adjacency list
         for(auto it:prerequisites){
             adj[it[1]].push_back(it[0]);
         }
-
-        //calculate indegrre of each node
         vector<int>indegree(n);
-       for(int i=0;i<n;i++){
-        for(auto it:adj[i]){
-            indegree[it]++;
+        for(int i=0;i<n;i++){
+            for(auto num:adj[i ]){
+                indegree[num]++;
+
+            }
         }
-       }
-       //check if indegree zero,,,then push the node into the ququq
+
         queue<int>q;
+        vector<int>topo;
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
                 q.push(i);
             }
         }
-        vector<int>topo;
+
         while(!q.empty()){
             int node=q.front();
+
             topo.push_back(node);
             q.pop();
-
-
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
+              for(auto z: adj[node]){
+                indegree[z]--;
+                if(indegree[z]==0){
+                    q.push(z);
                 }
-            }
-        }
+              }
 
+        }
         if(topo.size()==n)return true;
         return false;
+
+
 
         
     }
