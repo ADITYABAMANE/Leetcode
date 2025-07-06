5f4753e3d14ad3fbdd451daf9e8dab065f1b3fc5
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-07-06 18:48:53 +0530

    Time: 0 ms (100%), Space: 62.9 MB (48.2%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+       //using the sliding window problem
         int n=height.size();
         int left=0;
         int right=n-1;
-        int max_water=0;
+        int max_area=0;
         int area=0;
-        while(left<right ){
-            area=min(height[left],height[right])*(right-left);
-             max_water=max(max_water,area);
+        while(left<right){
+
+            
+            //area=l*b
+
+
+            area=(right-left) * min(height[left],height[right]);
+            max_area=max(max_area,area);
+
             if(height[left]>height[right]){
                 right--;
-            }
-            else {
-                left++;
-            }
-           
 
+            }
+            else{left++;
+            }
         }
-        return max_water;
+
+        return max_area;
+
+        
+
+        
     }
 };
