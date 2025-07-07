dfb148dfad95d9b082adb1222991d9692f153bce
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-07-07 23:56:45 +0530

    Time: 0 ms (100%), Space: 17.1 MB (34.23%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -3,40 +3,41 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        //{{row,col},time}
+        queue<pair<pair<int,int>,int>>q;
 
-       queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>>vis(n,vector<int>(m,0));
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if (grid[i][j]==2){
+                if(grid[i][j]==2){
                     q.push({{i,j},0});
-                    vis[i][j]=2;
+                    vis[i][j]=1;
                 }
-                
-
             }
         }
+
         int tm=0;
-       
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,1,0,-1};
         while(!q.empty()){
+            
+
+            int delrow[]={-1,0,1,0};
+            int delcol[]={0,-1,0,1};
             int r=q.front().first.first;
             int c=q.front().first.second;
             int t=q.front().second;
-             tm=max(tm,t);
+            tm=max(tm,t);
             q.pop();
 
             for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
+                int nrow=r+delrow[i];
+                int ncol=c+delcol[i];
+
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
                     q.push({{nrow,ncol},t+1});
                     vis[nrow][ncol]=2;
                 }
             }
+
 
         }
         for(int i=0;i<n;i++){
@@ -47,9 +48,10 @@ public:
             }
         }
         return tm;
-
-         
-
         
     }
-};
+};                
+                        // row-1,col    
+
+    // row,col-1             no          row,col+1
+                    //    row+1,col     
