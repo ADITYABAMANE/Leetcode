2c9ca585e83b366b42706daa352f0504009137d3
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-07-21 16:44:04 +0530

    Time: 30 ms (49.5%), Space: 50.1 MB (5.59%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int freq=1;
+        vector<char>ans;
+        ans.push_back(s[0]);
+        char prev=s[0];
+
+        for(int i=1;i<s.length();i++){
+            if(prev==s[i]){
+                freq++;
+            }
+            else{
+                prev=s[i];
+                freq=1;
+            }
+
+            if(freq<3){
+                ans.push_back(s[i]);
+
+            }
+
+            
+
+
+
+
+
+
+        }
+        string res="";
+        for(int i=0; i< ans.size();i++){
+
+            res+=ans[i];
+        }
+
+        return res;
+        
+    }
+};
