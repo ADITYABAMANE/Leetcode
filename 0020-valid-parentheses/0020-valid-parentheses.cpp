0ec958981dacbd661e1f90d95e214b7ae339d307
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-07-23 23:28:52 +0530

    Time: 2 ms (12.62%), Space: 8.8 MB (60.79%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        int n=s.length();
+        for(auto it:s){
+            if(it=='(' || it=='{' || it=='[' ){st.push(it);}
+            else{
+                if(st.empty()==true)return false;
+                else{
+                    char ch=st.top();
+                    st.pop();
+                    if(it==')' && ch=='(' ||  it=='}' && ch=='{' ||
+                    it==']' && ch=='[')continue;
+                    else return false;
+                }
+
+
+
+
+
+
+            }
+
+
+
+
+
+        }return st.empty();
+        
+
+
+
+
+
+
+
+
+    }
+};
