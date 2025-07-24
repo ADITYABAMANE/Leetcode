91f0af7296c746cef7be556344790b00eb538442
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-07-24 19:25:55 +0530

    Time: 25 ms (72.66%), Space: 24.4 MB (17.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,48 +1,64 @@
 class Solution {
-private:
-    void bfs(vector<vector<char>>& grid, int row, int col, vector<vector<int>>& vis) {
-        vis[row][col] = 1;
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<int, int>> q;
-        q.push({row, col});
+public:
 
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, -1, 0, 1};
 
-        while (!q.empty()) {
-            int crow = q.front().first;
-            int ccol = q.front().second;
-            q.pop();
+    void bfs(vector<vector<char>>& grid, vector<vector<int>>&vis,int row,int col){
+        queue<pair<int,int>>q;
+        vis[row][col]=1;
 
-            for (int i = 0; i < 4; i++) {
-                int nrow = crow + delrow[i];
-                int ncol = ccol + delcol[i];
+        int n=grid.size();
+        int m=grid[0].size();
 
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    !vis[nrow][ncol] && grid[nrow][ncol] == '1') {
-                    vis[nrow][ncol] = 1;
-                    q.push({nrow, ncol});
-                }
+
+        // vis[row][col]=1;
+        q.push({row,col});
+
+       int delrow[]={-1,0,1,0};
+       int delcol[]={0,-1,0,1};
+
+       while(!q.empty()){
+        int trow=q.front().first;
+        int tcol=q.front().second;
+
+        q.pop();
+        for(int i=0;i<4;i++){
+            int nrow=trow+delrow[i];
+            int ncol=tcol+delcol[i];
+
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
+                vis[nrow][ncol]=1;
+                q.push({nrow,ncol});
             }
         }
+
+
+
+       }
+
+
+
+
+
+
+
     }
 
-public:
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        int cnt = 0;
+        int n=grid.size();
+        int m= grid[0].size();
+        int cnt=0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
 
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < m; col++) {
-                if (!vis[row][col] && grid[row][col] == '1') {
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    bfs(grid,vis,i,j);
                     cnt++;
-                    bfs(grid, row, col, vis);
                 }
             }
         }
         return cnt;
+        
     }
 };
