b8ec52c09053afd2a2961e1952427fbd6ba2eb09
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-07-26 19:40:39 +0530

    Time: 0 ms (100%), Space: 10.8 MB (54.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
 
-    int fn(vector<int>& nums,int ind, vector<int>&dp,int start){
-        if(ind==start)return nums[ind];
-        if(ind<start)return 0;
+    int solve(vector<int>& nums,int ind, int n,vector<int>&dp){
 
-        if(dp[ind]!=-1)return dp[ind];
+        if(ind>n)return 0;
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+        int pick=nums[ind]+ solve(nums,ind+2,n,dp);
+        int np=0+solve(nums,ind+1,n,dp);
 
-        int take=nums[ind]+fn(nums,ind-2,dp,start);
-        int notake=0+fn(nums,ind-1,dp,start);
-
-        return dp[ind]=max(take,notake);
-         
-
-
-
-
-
-
-
+        return dp[ind] = max(pick,np);
 
     }
-
     int rob(vector<int>& nums) {
-         int n=nums.size();
-        // vector<int>dp(n,-1);
-       
-        //return fn(nums,n-1,dp);
+
+        int n=nums.size();
+
         if(n==1)return nums[0];
         if(n==2)return max(nums[0],nums[1]);
-
         vector<int>dp1(n,-1);
         vector<int>dp2(n,-1);
-        int case1=fn(nums,n-2,dp1,0);
-        int case2=fn(nums,n-1,dp2,1);
+       
 
-        return max(case1,case2);
+        int ans1=solve(nums,0,n-2,dp1);
+        int ans2=solve(nums, 1,n-1,dp2);
 
+        return max(ans1,ans2);
         
     }
 };
