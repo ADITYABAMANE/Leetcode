7df8c9cce9361e29452bf00a511b3dd04aaf1771
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-07-29 21:10:47 +0530

    Time: 0 ms (100%), Space: 18.4 MB (82.14%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,28 +10,61 @@
  */
 class Solution {
 public:
+
+    ListNode* DivideLL(vector<ListNode*>& lists,int l,int r){
+        if(l>r)return NULL;
+        if(l==r)return lists[l];
+
+        int n=lists.size();
+
+        int mid=l+(r-l)/2;
+
+        ListNode*L1=DivideLL(lists,l,mid);
+        ListNode*L2=DivideLL(lists,mid+1,r);
+
+         return mergetwolists(L1, L2);
+
+
+        
+
+
+
+
+
+    }
+    ListNode*mergetwolists(ListNode*l1, ListNode*l2){
+        if(l1==NULL)return l2;
+        if(l2==NULL)return l1;
+        
+
+        if(l1->val<=l2->val){
+            l1->next= mergetwolists(l1->next,l2);
+
+            // l1=l1->next;
+            return l1;
+
+
+        }
+        else{
+            l2->next=mergetwolists(l2->next,l1);
+
+           
+            return l2;
+
+
+        }
+        // return NULL;
+
+    }
+
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty())return NULL;
+        int n=lists.size();
 
-        vector<int>values;
-        for(ListNode* list:lists){
-            while(list){
-                values.push_back(list->val);
-                list=list->next;
-            }
-        }
-
-        sort(values.begin(),values.end());
-
-        ListNode* dummy= new ListNode(-1);
-        ListNode* curr=dummy;
-
-        for(auto it: values){
-            curr->next=new ListNode(it);
-            curr=curr->next;
+       return  DivideLL(lists,0,n-1);
 
 
-        }
-        return dummy->next;
-
+        
     }
 };
