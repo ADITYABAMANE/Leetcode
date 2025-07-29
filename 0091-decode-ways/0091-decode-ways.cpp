da06d6e60aaf0ce2e1716101be9492733bc79089
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2025-07-29 23:09:00 +0530

    Time: 0 ms (100%), Space: 8.6 MB (75.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
 
-    int solve(string &str,int ind, int &n,vector<int>&dp){
+    int solve(string &s,int ind, vector<int>&dp){
+        int n=s.length();
+        if(s[ind]=='0'){
+            return 0;
+        }
         if(ind==n){
             return 1;
-
-        }
-        if(str[ind]=='0'){
-            return 0;
         }
         if(dp[ind]!=-1){
             return dp[ind];
         }
-        int pick_one_digit=solve(str, ind+1,n,dp);
-        int pick_two_digit=0;
+
+        int take_one_digit=solve(s,ind+1,dp);
+        int take_two_digit=0;
 
         if(ind+1<n){
 
-        if(str[ind]=='1' || (str[ind]=='2' && str[ind+1]<='6')){
+        if(s[ind]=='1' || s[ind]=='2' && s[ind+1]<='6'){
+         take_two_digit=solve(s,ind+2,dp);}}
 
-        pick_two_digit=solve(str, ind+2,n,dp);}}
-
-
-        return dp[ind]=pick_one_digit + pick_two_digit;
-
+        return  dp[ind]=take_one_digit + take_two_digit;
 
     }
-
     int numDecodings(string s) {
         int n=s.length();
         vector<int>dp(n,-1);
+        return solve(s,0,dp);
 
-        return solve (s, 0, n,dp);
+
+        
         
     }
 };
