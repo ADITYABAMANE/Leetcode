675f86720e7ea665ae99b569922c8ed06d6a1cb6
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2026-04-28 01:30:03 +0530

    Time: 0 ms (100%), Space: 27.4 MB (16.12%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
+
+    void prefixlm(vector<int>& height,vector<int>&leftmax,int n){
+        
+        
+
+        leftmax[0]=height[0];
+
+        for(int i=1;i<n;i++){
+            leftmax[i]=max(leftmax[i-1],height[i]);
+        }
+
+
+
+
+    }
+    void suffixrm(vector<int>& height,vector<int>&rightmax,int n){
+        
+         rightmax[n-1]=height[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            rightmax[i]=max(rightmax[i+1],height[i]);
+        }
+
+    }
     int trap(vector<int>& height) {
         int n=height.size();
-        int l=0;
-        int r = n-1;
 
-        int leftmax=height[0];
-        int rightmax=height[n-1];
+        //if i am at a certain point, to calculate the water stored at that point , i need the leftmax till that point and right max till that point, and then will take the min of them both - height of thr bar
+
+        vector<int>leftmax(n);
+        vector<int> rightmax(n);
+        prefixlm(height,leftmax,n);
+        suffixrm(height, rightmax, n);
         int water=0;
 
-        while(l<r){
-
-            if(leftmax<rightmax){
-                l++;
-                leftmax=max(leftmax, height[l]);
-                water+=leftmax-height[l];
-
-
-            }
-            else{
-                r--;
-                rightmax=max(rightmax, height[r]);
-                water+=rightmax-height[r];
-            }
-
+        for(int i=0;i<n;i++){
+            water+=min(leftmax[i],rightmax[i] )-height[i];
 
 
 
         }
         return water;
 
-        
+
         
     }
 };
