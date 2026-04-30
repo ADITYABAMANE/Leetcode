75d16ea3ac670f6bd368abdd839cc1f896d9d1bf
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2026-05-01 01:11:29 +0530

    Time: 11 ms (27.62%), Space: 10.9 MB (39.41%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
- int maxlen=0;
     int characterReplacement(string s, int k) {
+
         int n=s.length();
 
-        int i=0;
-        int j=i;
-        map<int,int>mp;
-        int maxfreq=0;
         int maxlen=0;
+        int chardiff=0;
+        int maxfreq=0;
+
+        unordered_map<char,int >mp;
+
+        int i=0;int j=0;
+
         while(j<n){
             mp[s[j]]++;
-
             maxfreq=max(maxfreq,mp[s[j]]);
-            int chardiff=(j-i+1)-maxfreq;
+            chardiff=(j-i+1)-maxfreq;
 
             if(chardiff>k){
                 mp[s[i]]--;
@@ -22,10 +24,10 @@ public:
             maxlen=max(maxlen,j-i+1);
             j++;
 
+            
         }
         return maxlen;
-       
-        
+
         
     }
 };
